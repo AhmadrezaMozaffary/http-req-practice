fa67b120e04fe2d9da58373b6114a5cb3833728e
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-07-02 22:33:51 +0430

    Use useEffect and useCallback hooks to send HTTP req

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,7 +8,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = async () => {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -32,7 +32,11 @@ function App() {
     }
 
     setIsLoading(false);
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>No movies !</p>;
   if (movies.length > 0) content = <MoviesList movies={movies} />;
